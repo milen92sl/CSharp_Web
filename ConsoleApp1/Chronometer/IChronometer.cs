5bd21eb2e1327a6000c1fa5d04c7a5468468026b
Milen Ivanov <milensl92@gmail.com>
2021-05-23 18:23:16 +0300

    first commit from web

---
+++ ConsoleApp1/Chronometer/IChronometer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public interface IChronometer
+    {
+        string GetTime { get;}
+        List<string> Laps {get;}
+        void Start();
+        void Stop();
+        string Lap();
+        void Reset();
+
+    }
+}
---
+++ ConsoleApp1/Chronometer/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chronometer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var chronometer = new ChronometerAsync();
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "start")
+                {
+                    chronometer.Start();
+                }
+                else if (command == "stop")
+                {
+                    chronometer.Stop();
+                }
+                else if (command == "lap")
+                {
+                    Console.WriteLine(chronometer.Lap());
+                }
+                else if (command == "laps")
+                {
+                    var laps = chronometer.Laps;
+                    if (laps.Count > 0)
+                    {
+
+                        Console.WriteLine("Laps:");
+                        for (int i = 0; i < laps.Count; i++)
+                        {
+                            Console.WriteLine($"{i}. {laps[i].ToString()}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Laps:no laps");
+                    }
+                }
+                else if (command == "time")
+                {
+                    var time = chronometer.GetTime;
+                    Console.WriteLine(time);
+                }
+                else if (command == "reset")
+                {
+                    chronometer.Reset();
+                }
+                else if (command == "exit")
+                {
+                    chronometer.Stop();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Function not implemented!");
+                }
+            }
+        }
+    }
+}
+
